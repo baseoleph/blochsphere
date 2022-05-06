1c9545e5730981d3fc20132586835a4efe7be31a
baseoleph <baseoleph@gmail.com>
2022-05-06 17:57:23 +0300

    fix: add return to non-void function

--- src/utility.cpp
+++ src/utility.cpp
@@ -46,25 +46,27 @@ complex parseStrToComplex(const QString &str) {
 
     if (str.contains(rxp3)) {
         if (rxp3.capturedTexts()[1] == "+" || rxp3.capturedTexts()[1] == "") {
-            return complex(0.0, 1.0);
+            return {0.0, 1.0};
         } else if (rxp3.capturedTexts()[1] == "-") {
-            return complex(0.0, -1.0);
+            return {0.0, -1.0};
         } else {
-            return complex(0.0, rxp3.capturedTexts()[1].toDouble());
+            return {0.0, rxp3.capturedTexts()[1].toDouble()};
         }
     } else if (str.contains(rxp1)) {
         if (rxp1.capturedTexts()[2] == "+") {
-            return complex(rxp1.capturedTexts()[1].toDouble(), 1.0);
+            return {rxp1.capturedTexts()[1].toDouble(), 1.0};
         } else if (rxp1.capturedTexts()[2] == "-") {
-            return complex(rxp1.capturedTexts()[1].toDouble(), -1.0);
+            return {rxp1.capturedTexts()[1].toDouble(), -1.0};
         } else if (rxp1.capturedTexts().size() > 2) {
-            return complex(rxp1.capturedTexts()[1].toDouble(), rxp1.capturedTexts()[2].toDouble());
+            return {rxp1.capturedTexts()[1].toDouble(), rxp1.capturedTexts()[2].toDouble()};
         } else {
-            return complex(0, rxp1.capturedTexts()[1].toDouble());
+            return {0, rxp1.capturedTexts()[1].toDouble()};
         }
     } else if (str.contains(rxp2)) {
-        return complex(rxp2.capturedTexts()[1].toDouble(), 0.0);
+        return {rxp2.capturedTexts()[1].toDouble(), 0.0};
     }
+
+    return {0, 0};
 }
 
 QString parseComplexToStr(complex c, int d) {
